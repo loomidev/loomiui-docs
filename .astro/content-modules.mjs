9cfeca58c429c01dd2dd0aa8423c33dc70bbc01c
Michael K. Ocansey <kabutey@gmail.com>
2026-06-26 18:41:34 +0000

    Removed files that should not be tracked

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,5 +1,6 @@
 
 export default new Map([
+["src/content/docs/404.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2F404.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/architecture.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Farchitecture.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/customization.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fcustomization.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/index-not-used.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex-not-used.mdx&astroContentModuleFlag=true")],
