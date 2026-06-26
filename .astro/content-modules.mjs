7cef2e3f18a50ac1a8c617841795f1f0369483c7
Michael K. Ocansey <kabutey@gmail.com>
2026-06-26 19:03:58 +0000

    fix: remove .astro from gitignore

--- .astro/content-modules.mjs
+++
@@ -1,8 +0,0 @@
-
-export default new Map([
-["src/content/docs/404.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2F404.mdx&astroContentModuleFlag=true")],
-["src/content/docs/architecture.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Farchitecture.mdx&astroContentModuleFlag=true")],
-["src/content/docs/customization.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fcustomization.mdx&astroContentModuleFlag=true")],
-["src/content/docs/index-not-used.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex-not-used.mdx&astroContentModuleFlag=true")],
-["src/content/docs/mcp-server.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fmcp-server.mdx&astroContentModuleFlag=true")]]);
-		
